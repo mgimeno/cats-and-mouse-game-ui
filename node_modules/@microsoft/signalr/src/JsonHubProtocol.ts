53c62b6a5ccf587c9b5a8869ad5a20e635636707
mgimeno <mgimeno@gmail.com>
2024-11-30 12:28:50 +0100

    Update angular 2

--- node_modules/@microsoft/signalr/src/JsonHubProtocol.ts
+++ node_modules/@microsoft/signalr/src/JsonHubProtocol.ts
@@ -1,7 +1,7 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
-import { CompletionMessage, HubMessage, IHubProtocol, InvocationMessage, MessageType, StreamItemMessage } from "./IHubProtocol";
+import { AckMessage, CompletionMessage, HubMessage, IHubProtocol, InvocationMessage, MessageType, SequenceMessage, StreamItemMessage } from "./IHubProtocol";
 import { ILogger, LogLevel } from "./ILogger";
 import { TransferFormat } from "./ITransport";
 import { NullLogger } from "./Loggers";
@@ -15,7 +15,7 @@ export class JsonHubProtocol implements IHubProtocol {
     /** @inheritDoc */
     public readonly name: string = JSON_HUB_PROTOCOL_NAME;
     /** @inheritDoc */
-    public readonly version: number = 1;
+    public readonly version: number = 2;
 
     /** @inheritDoc */
     public readonly transferFormat: TransferFormat = TransferFormat.Text;
@@ -50,19 +50,25 @@ export class JsonHubProtocol implements IHubProtocol {
             }
             switch (parsedMessage.type) {
                 case MessageType.Invocation:
-                    this.isInvocationMessage(parsedMessage);
+                    this._isInvocationMessage(parsedMessage);
                     break;
                 case MessageType.StreamItem:
-                    this.isStreamItemMessage(parsedMessage);
+                    this._isStreamItemMessage(parsedMessage);
                     break;
                 case MessageType.Completion:
-                    this.isCompletionMessage(parsedMessage);
+                    this._isCompletionMessage(parsedMessage);
                     break;
                 case MessageType.Ping:
                     // Single value, no need to validate
                     break;
                 case MessageType.Close:
                     // All optional values, no need to validate
+                    break;
+                case MessageType.Ack:
+                    this._isAckMessage(parsedMessage);
+                    break;
+                case MessageType.Sequence:
+                    this._isSequenceMessage(parsedMessage);
                     break;
                 default:
                     // Future protocol changes can add message types, old clients can ignore them
@@ -84,35 +90,47 @@ export class JsonHubProtocol implements IHubProtocol {
         return TextMessageFormat.write(JSON.stringify(message));
     }
 
-    private isInvocationMessage(message: InvocationMessage): void {
-        this.assertNotEmptyString(message.target, "Invalid payload for Invocation message.");
+    private _isInvocationMessage(message: InvocationMessage): void {
+        this._assertNotEmptyString(message.target, "Invalid payload for Invocation message.");
 
         if (message.invocationId !== undefined) {
-            this.assertNotEmptyString(message.invocationId, "Invalid payload for Invocation message.");
+            this._assertNotEmptyString(message.invocationId, "Invalid payload for Invocation message.");
         }
     }
 
-    private isStreamItemMessage(message: StreamItemMessage): void {
-        this.assertNotEmptyString(message.invocationId, "Invalid payload for StreamItem message.");
+    private _isStreamItemMessage(message: StreamItemMessage): void {
+        this._assertNotEmptyString(message.invocationId, "Invalid payload for StreamItem message.");
 
         if (message.item === undefined) {
             throw new Error("Invalid payload for StreamItem message.");
         }
     }
 
-    private isCompletionMessage(message: CompletionMessage): void {
+    private _isCompletionMessage(message: CompletionMessage): void {
         if (message.result && message.error) {
             throw new Error("Invalid payload for Completion message.");
         }
 
         if (!message.result && message.error) {
-            this.assertNotEmptyString(message.error, "Invalid payload for Completion message.");
+            this._assertNotEmptyString(message.error, "Invalid payload for Completion message.");
         }
 
-        this.assertNotEmptyString(message.invocationId, "Invalid payload for Completion message.");
+        this._assertNotEmptyString(message.invocationId, "Invalid payload for Completion message.");
     }
 
-    private assertNotEmptyString(value: any, errorMessage: string): void {
+    private _isAckMessage(message: AckMessage): void {
+        if (typeof message.sequenceId !== 'number') {
+            throw new Error("Invalid SequenceId for Ack message.");
+        }
+    }
+
+    private _isSequenceMessage(message: SequenceMessage): void {
+        if (typeof message.sequenceId !== 'number') {
+            throw new Error("Invalid SequenceId for Sequence message.");
+        }
+    }
+
+    private _assertNotEmptyString(value: any, errorMessage: string): void {
         if (typeof value !== "string" || value === "") {
             throw new Error(errorMessage);
         }
