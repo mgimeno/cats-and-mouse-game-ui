368bd20aafcaaa6f39a3b76a801ff8c0aa87b67b
mgimeno <mgimeno@gmail.com>
2025-03-22 20:37:26 +0100

    Dev env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  name: "production",
-  apiUrl: "http://cats-and-mouse-api.marcosgimeno.com/api/",
-  apiGameHubUrl: "http://cats-and-mouse-api.marcosgimeno.com/gameHub",
-  websiteUrl: "http://cats-and-mouse.marcosgimeno.com/",
+  production: false,
+  name: "development",
+  apiUrl: "http://localhost:51000/",
+  apiGameHubUrl: "http://localhost:51000/gameHub",
+  websiteUrl: "http://localhost:4200/",
   localStoragePrefix: "cat-and-mouse-game_",
 };
 
