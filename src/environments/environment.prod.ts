1213e416e3430b71b1c8584c2e03faa711dc2189
mgimeno <mgimeno@gmail.com>
2025-05-10 20:22:06 +0200

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   name: "production",
-  apiUrl: "https://www.marcosgimeno.com:51000/api/",
-  apiGameHubUrl: "https://www.marcosgimeno.com:51000/gameHub",
+  apiUrl: "https://www.marcosgimeno.com:53000/api/",
+  apiGameHubUrl: "https://www.marcosgimeno.com:53000/gameHub",
   websiteUrl: "https://www.marcosgimeno.com/cats-and-mouse",
   localStoragePrefix: "cat-and-mouse-game_",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   name: "development",
-  apiUrl: "http://localhost:51000/",
-  apiGameHubUrl: "http://localhost:51000/gameHub",
+  apiUrl: "http://localhost:53000/",
+  apiGameHubUrl: "http://localhost:53000/gameHub",
   websiteUrl: "http://localhost:4200/",
   localStoragePrefix: "cat-and-mouse-game_",
 };
