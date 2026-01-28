ed726e0d184bea33ea693336839c0278a7d2c8a5
mgimeno <mgimeno@gmail.com>
2026-01-28 12:46:13 +0100

    New API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   name: "production",
-  apiUrl: "https://www.marcosgimeno.com:53000/api/",
-  apiGameHubUrl: "https://www.marcosgimeno.com:53000/gameHub",
+  apiUrl: "https://api.cats-and-mouse.marcosgimeno.com/api/",
+  apiGameHubUrl: "https://api.cats-and-mouse.marcosgimeno.com/gameHub",
   websiteUrl: "https://www.marcosgimeno.com/cats-and-mouse",
   localStoragePrefix: "cat-and-mouse-game_",
 };
