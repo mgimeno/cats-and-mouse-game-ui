ca2f15a6c1b508e4f2b557acbd630dfd7a3e436a
mgimeno <mgimeno@gmail.com>
2026-01-28 13:55:09 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayGameComponent } from './components/game/play-game/play-game.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'play', component: PlayGameComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
